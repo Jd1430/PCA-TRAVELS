ccba347997a07b5e4de191aafcaf75a8af615f58
Jayanth Devaraj Gowda <jayanthdevarajgowda@gmail.com>
2025-07-24 10:58:32 +0530

    Update config.js

--- frontend/src/api/config.js
+++ frontend/src/api/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'https://pca-travels.onrender.com', // http://localhost:5000/api
+  baseURL: 'https://pca-travels.onrender.com/api', // http://localhost:5000/api
   headers: {
     'Content-Type': 'application/json'
   }
